9390e493620c1bebdf74e40a7049edd37b4436ee
aleksandar angelevski <stotka123@yahoo.com>
2021-12-14 09:43:30 +0100

    LoginForm-set

---
+++ src/components/SingupForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input
+      type="text"
+      required
+      placeholder="display name"
+      v-model="displayName"
+    />
+    <input type="email" required placeholder="email" v-model="email" />
+    <input type="password" required placeholder="password" v-model="password" />
+    <button>Sing up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    //refs
+    const displayname = ref("");
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = () => {
+      console.log(displayname.value, email, password.value);
+    };
+
+    return { displayname, email, password, handleSubmit };
+  },
+};
+</script>
+
+<style></style>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,16 +1,41 @@
 <template>
   <div class="welcome container">
-    <p>welcome</p>
+    <SingupForm />
+    <LoginForm />
   </div>
 </template>
 
 <script>
-export default {};
+import SingupForm from "@/components/SingupForm";
+import LoginForm from "../components/LoginForm.vue";
+export default {
+  components: {
+    SingupForm,
+    LoginForm,
+  },
+};
 </script>
 
 <style>
 .welcome {
   text-align: center;
   padding: 20px 0;
+}
+.welcome form {
+  width: 300px;
+  margin: 20px 0 10px;
+}
+.welcome label {
+  display: block;
+  margin: 20px 0 10px;
+}
+.welcome input {
+  width: 100%;
+  padding: 10px;
+  border-radius: 20px;
+  border: 1 px solid #eee;
+  outline: none;
+  color: antiquewhite;
+  margin: 10px auto;
 }
 </style>
